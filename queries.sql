43803d8b6bde8d1bed331e7a03acc56d4b76a27e
Matthew Weiper <mattweiper@gmail.com>
2021-06-05 11:36:18 -0400

    Added SQL and final edits

---
+++ queries.sql
@@ -0,0 +1,12 @@
+CREATE TABLE sales(
+	index INT PRIMARY KEY,
+	Rank INT,
+	Name VARCHAR,
+	Platform VARCHAR,
+	Genre VARCHAR,
+	Global_Sales DECIMAL,
+	ESRB_Rating VARCHAR
+);
+
+
+SELECT * FROM sales;
